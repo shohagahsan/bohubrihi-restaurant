3e2627e7f3bd42c39648ec773e0258cd99b941f2
Shohag Ahsan <theShohagAhsan@gmail.com>
2021-08-10 17:43:53 +0600

    Creating Menu Component

---
+++ src/data/dishes.js
@@ -0,0 +1,189 @@
+const DISHES = [
+    {
+        id: 0,
+        name: 'Burger',
+        image: 'assets/images/0burger.jpg',
+        category: 'meal',
+        label: 'Hot',
+        price: '199',
+        description: '',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: '',
+                author: 'Shanto',
+                date: '2018-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 1,
+                rating: 4,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 2,
+                rating: 3,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 3,
+                rating: 2,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 4,
+                rating: 1,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: 'Pizza 1',
+        image: 'assets/images/1pizza.jpg',
+        category: 'meal',
+        label: 'Hot',
+        price: '199',
+        description: '',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: '',
+                author: 'Shanto',
+                date: '2018-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 1,
+                rating: 4,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 2,
+                rating: 3,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 3,
+                rating: 2,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 4,
+                rating: 1,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'French Fries',
+        image: 'assets/images/2fries.jpg',
+        category: 'meal',
+        label: 'Hot',
+        price: '199',
+        description: '',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: '',
+                author: 'Shanto',
+                date: '2018-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 1,
+                rating: 4,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 2,
+                rating: 3,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 3,
+                rating: 2,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 4,
+                rating: 1,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'Chiken Bar B Q Pizza',
+        image: 'assets/images/3cheken.jpg',
+        category: 'meal',
+        label: 'Hot',
+        price: '199',
+        description: '',
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: '',
+                author: 'Shanto',
+                date: '2018-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 1,
+                rating: 4,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 2,
+                rating: 3,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 3,
+                rating: 2,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            },
+            {
+                id: 4,
+                rating: 1,
+                comment: '',
+                author: 'Shanto',
+                date: '2017-10-16T17:57:28.556094Z'
+            }
+        ]
+    }
+
+]
+
+export default DISHES;
